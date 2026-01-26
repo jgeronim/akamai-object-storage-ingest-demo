084b64e1e8a7e6fa7a7008324e8e6db4973a8e64
Jorge G <233847607+jgeronim@users.noreply.github.com>
2026-01-26 13:46:33 -0800

    adding two files

--- backend/s3Client.js
+++ backend/s3Client.js
@@ -4,7 +4,7 @@ export function makeClient() {
   return new S3Client({
     region: process.env.REGION,
     endpoint: process.env.ENDPOINT,
-    forcePathStyle: true, // Critical for Akamai/Linode Objects
+    forcePathStyle: true, // Critical for Akamai Objects
     credentials: {
       accessKeyId: process.env.AWS_ACCESS_KEY_ID,
       secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
